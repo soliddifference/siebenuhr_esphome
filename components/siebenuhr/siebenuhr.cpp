9dbb461b62c334f57790d503ecf34f4e3af40904
Yannick Suter <yannick.suter@gmail.com>
2024-11-28 19:20:40 +0100

    implementing onboard blinky

--- components/siebenuhr/siebenuhr.cpp
+++ components/siebenuhr/siebenuhr.cpp
@@ -6,9 +6,21 @@ namespace siebenuhr {
 
 static const char *const TAG = "siebenuhr.display";
 
-void SiebenuhrDisplay::setup() {}
+void Siebenuhr::setup() {
+  ESP_LOGCONFIG(TAG, "Setting up Siebenuhr Component...");
+  pinMode(pin_, OUTPUT);
+}
 
-void SiebenuhrDisplay::loop() {}
+void Siebenuhr::loop() {
+  static unsigned long last_toggle = 0;
+  unsigned long now = millis();
+  if (now - last_toggle > 1000) {  // Toggle every second
+    m_state = !m_state;
+    digitalWrite(pin_, m_state);
+    ESP_LOGD(TAG, "Toggling LED: %s", state_ ? "ON" : "OFF");
+    last_toggle = now;
+  }
+}
 
 }  // namespace siebenuhr
 }  // namespace esphome
